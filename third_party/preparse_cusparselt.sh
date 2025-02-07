d6abecb357b7edd3e6f174d6d932a814ea24c0b9
fan1997 <2479031300@qq.com>
2025-02-07 11:36:41 +0800

    Preparse cusparselt

--- third_party/preparse_cusparselt.sh
+++ third_party/preparse_cusparselt.sh
@@ -14,8 +14,3 @@ tar -xf libcusparse_lt-linux-x86_64-0.5.0.1-archive.tar.xz --strip-components=1
 # 清理下载的压缩包
 rm libcusparse_lt-linux-x86_64-0.5.0.1-archive.tar.xz
 
-# 设置环境变量
-echo "Setting up environment variables..."
-export CPLUS_INCLUDE_PATH=${SpInfer_HOME}/third_party/cusparselt/include:$CPLUS_INCLUDE_PATH
-export LIBRARY_PATH=${SpInfer_HOME}/third_party/cusparselt/lib:$LIBRARY_PATH
-export LD_LIBRARY_PATH=${SpInfer_HOME}/third_party/cusparselt/lib:$LD_LIBRARY_PATH
