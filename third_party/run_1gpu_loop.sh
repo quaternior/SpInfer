a0a2cf29d11cb6eb4f89c11dc917ec8d0554b447
xxyux <1650459510@qq.com>
2025-02-10 10:54:53 +0800

    add deepspeed script

--- third_party/run_1gpu_loop.sh
+++ third_party/run_1gpu_loop.sh
@@ -3,6 +3,9 @@
 # Define the configuration file path
 CONFIG_FILE="${SpInfer_HOME}/third_party/FasterTransformer/examples/cpp/multi_gpu_gpt/gpt_config.ini"
 
+sed -i -E "s/(tensor_para_size=)[0-9]+/\11/" "$CONFIG_FILE"
+sed -i -E "s#(model_dir\s*=\s*).*#\1/data2/fanruibo/models/opt-13b/spinfer-model/1-gpu#" "$CONFIG_FILE"
+
 # Define the output directory path
 OUTPUT_DIR="${SpInfer_HOME}/third_party/FasterTransformer/OutputFile_1gpu_our_60_inlen64"
 
@@ -27,7 +30,7 @@ for batch_size in "${BATCH_SIZES[@]}"; do
     OUTPUT_FILE="$OUTPUT_DIR/output_batch_${batch_size}_output_len_${output_len}.log"
 
     # Run the command and save the output to the file
-    CUDA_VISIBLE_DEVICES=7 mpirun -n 1 --allow-run-as-root ./bin/multi_gpu_gpt_example > "$OUTPUT_FILE" 2>&1
+    CUDA_VISIBLE_DEVICES=0 mpirun -n 1 --allow-run-as-root ./bin/multi_gpu_gpt_example > "$OUTPUT_FILE" 2>&1
 
     # Print the status
     echo "Completed run with batch_size=$batch_size, output_len=$output_len. Output saved to $OUTPUT_FILE"
--- third_party/run_4gpu_loop.sh
+++ third_party/run_4gpu_loop.sh
@@ -3,11 +3,11 @@
 # Define the configuration file path
 CONFIG_FILE="${SpInfer_HOME}/third_party/FasterTransformer/examples/cpp/multi_gpu_gpt/gpt_config.ini"
 
-sed -i -E "s/(tensor_para_size=)[0-9]+/\12/" "$CONFIG_FILE"
-sed -i -E "s#(model_dir\s*=\s*).*#\1/data2/fanruibo/models/opt-30b/spinfer-model/2-gpu#" "$CONFIG_FILE"
+sed -i -E "s/(tensor_para_size=)[0-9]+/\14/" "$CONFIG_FILE"
+sed -i -E "s#(model_dir\s*=\s*).*#\1/data2/fanruibo/models/opt-30b/spinfer-model/4-gpu#" "$CONFIG_FILE"
 
 # Define the output directory path
-OUTPUT_DIR="${SpInfer_HOME}/third_party/FasterTransformer/OutputFile_2gpu_our_60_inlen64"
+OUTPUT_DIR="${SpInfer_HOME}/third_party/FasterTransformer/OutputFile_4gpu_our_60_inlen64"
 
 # Create the output directory if it does not exist
 mkdir -p "$OUTPUT_DIR"
@@ -30,7 +30,7 @@ for batch_size in "${BATCH_SIZES[@]}"; do
     OUTPUT_FILE="$OUTPUT_DIR/output_batch_${batch_size}_output_len_${output_len}.log"
 
     # Run the command and save the output to the file
-    CUDA_VISIBLE_DEVICES=0,1 mpirun -n 2 --allow-run-as-root ./bin/multi_gpu_gpt_example > "$OUTPUT_FILE" 2>&1
+    CUDA_VISIBLE_DEVICES=0,1,2,3 mpirun -n 4 --allow-run-as-root ./bin/multi_gpu_gpt_example > "$OUTPUT_FILE" 2>&1
 
     # Print the status
     echo "Completed run with batch_size=$batch_size, output_len=$output_len. Output saved to $OUTPUT_FILE"
